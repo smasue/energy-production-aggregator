3554334e404ac1b5dbddd1de730900cdadf172f4
smasue <samuel.masue@gmail.com>
2020-09-04 14:58:19 +0200

    add main class and first unit dummy test

---
+++ src/test/java/bcm/energy/aggregator/AggregatorTest.java
@@ -0,0 +1,12 @@
+package bcm.energy.aggregator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AggregatorTest {
+    @Test
+    void testProject() {
+        assertTrue(true);
+    }
+}
